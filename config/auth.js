eb566a386b49845d14028d4b2381571fde2adcd7
Rubén Salinas <rubensalinasgarcia@gmail.com>
2015-11-29 21:30:44 +0100

    Prod config

--- config/auth.js
+++ config/auth.js
@@ -1,12 +1,12 @@
 module.exports = {
 	'facebookAuth': {
-		'clientID': '1723773851217494',
-		'clientSecret': '9ddb276cde1cd0ad3086d265ff9daa71',
-		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+		'clientID': '436648999859063',
+		'clientSecret': 'ba34d955d9f46d23bd3560c876adec73',
+		'callbackURL': 'http://baby-diary.herokuapp.com/auth/facebook/callback'
 	},
 	'googleAuth' : {
-        'clientID'      : '278672745152-f6eo59h8bcth80jt5ckuv0cm0jlhogq6.apps.googleusercontent.com',
-        'clientSecret'  : 'xTZ65F9jDtDEkWDkVtu5g2q0',
-        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+        'clientID'      : '472390506196-ed7d1ec9dtijbutaub04ae13bsdn2sv6.apps.googleusercontent.com',
+        'clientSecret'  : 'ny_1mC9wIXyyBOWfNwFSrJci',
+        'callbackURL'   : 'http://baby-diary.herokuapp.com/auth/google/callback'
     }
 }
