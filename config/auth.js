36ab155d8ed216c33ef92c0726a14c8e91fc7d10
Rubén Salinas <rubensalinasgarcia@gmail.com>
2015-11-29 12:37:30 +0100

    Auth file

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+module.exports = {
+	'facebookAuth': {
+		'clientID': '1723773851217494',
+		'clientSecret': '9ddb276cde1cd0ad3086d265ff9daa71',
+		'callbackURL': 'http://localhost:8080/auth/facebook/callback'
+	},
+	'googleAuth' : {
+        'clientID'      : '278672745152-f6eo59h8bcth80jt5ckuv0cm0jlhogq6.apps.googleusercontent.com',
+        'clientSecret'  : 'xTZ65F9jDtDEkWDkVtu5g2q0',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+}
