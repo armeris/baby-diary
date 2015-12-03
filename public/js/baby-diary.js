58e0dc6fcc69b71827bf88d891d9f3112a6707fe
Rubén Salinas <rubensalinasgarcia@gmail.com>
2015-12-03 23:51:28 +0100

    Selector next steps

--- public/js/baby-diary.js
+++ public/js/baby-diary.js
@@ -16,9 +16,28 @@ $(function(){
 	});
 	
 	$('a#addEvent').click(function(){
-		$("div.jumbotron.startEventCreation h1").toggle();
-		$("div.jumbotron.startEventCreation div.food-box").css('display','block');
-		$("div.jumbotron.startEventCreation div.diaper-box").css('display','block');
+		$('div.jumbotron.startEventCreation h1').toggle();
+		$('div.jumbotron.startEventCreation div.food-box').toggle();
+		$('div.jumbotron.startEventCreation div.diaper-box').toggle();
 		return false;
+	});
+	
+	$('div.selector').click(function(){
+		if($(this).hasClass('selected')){
+			$(this).removeClass('selected');
+			$('.group-selector.'+$(this).attr('data-type')).children().hide();
+		}else{
+			$(this).siblings().removeClass('selected');
+			var parentIndex = $('.jumbotron.startEventCreation').children('.group-selector').index($(this).parent());
+			$(this).parent().siblings().each(function(index, item){
+				if(index > parentIndex){
+					$(item).children().hide();
+					$(item).children().removeClass('selected');
+				}
+			});
+			$(this).addClass('selected');
+			$('input[name='+$(this).attr('data-field')+']').val($(this).attr('data-type'));
+			$('.group-selector.'+$(this).attr('data-type')).children().show();
+		}
 	});
 });
