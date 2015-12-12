03a67a0689d850c937e4459d4411d6a8e29c7aee
Rubén Salinas <rubensalinasgarcia@gmail.com>
2015-12-12 11:37:13 +0100

    New URL

--- config/auth.js
+++ config/auth.js
@@ -2,11 +2,11 @@ module.exports = {
 	'facebookAuth': {
 		'clientID': '436648999859063',
 		'clientSecret': 'ba34d955d9f46d23bd3560c876adec73',
-		'callbackURL': 'http://baby-diary.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'http://www.laagendadelbebe.es/auth/facebook/callback'
 	},
 	'googleAuth' : {
         'clientID'      : '472390506196-ed7d1ec9dtijbutaub04ae13bsdn2sv6.apps.googleusercontent.com',
         'clientSecret'  : 'ny_1mC9wIXyyBOWfNwFSrJci',
-        'callbackURL'   : 'http://baby-diary.herokuapp.com/auth/google/callback'
+        'callbackURL'   : 'http://www.laagendadelbebe.es/auth/google/callback'
     }
 }
