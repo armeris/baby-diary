6b5770612046ddec834f9ecbf3a63da91f1af882
Rubén Salinas <rubensalinasgarcia@gmail.com>
2016-01-01 13:13:55 +0100

    Fix graphs

--- app/model/baby.js
+++ app/model/baby.js
@@ -38,10 +38,10 @@ babySchema.statics.weekAggregatedData = function(babyId, callback){
 		today.setHours(0,0,0,0);
 		var week = 60*60*24*7*1000;
     for(var i=0; i<this.events.length; i++){
-			var eventTime = this.events[i].date.getTime() + (this.events[i].date.getTimezoneOffset() * 60000);
+			var eventTime = this.events[i].date.getTime();
 			var diff = today.getTime() - eventTime; 
 			if(diff <= week){		
-				var key = {id:this.id,name:this.name,event:this.events[i].eventClass, date: new Date(eventTime).getDate()+'/'+(new Date(eventTime).getMonth()+1)+'/'+new Date(eventTime).getFullYear()};
+				var key = {id:this._id,event:this.events[i].eventClass, date: new Date(eventTime).getDate()+'/'+(new Date(eventTime).getMonth()+1)+'/'+new Date(eventTime).getFullYear()};
 				var value = this.events[i].eventAmount;
 				emit(key,value);
 			}
