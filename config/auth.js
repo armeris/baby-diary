ec6e691eaf9740c5271bbb35b72b6f7a82f1aa61
Rubén Salinas <rubensalinasgarcia@gmail.com>
2016-01-01 13:38:16 +0100

    Prod config

---
+++ config/auth.js
@@ -0,0 +1,12 @@
+modules.exports = {
+	'facebookAuth': {
+		'clientID': '436648999859063',
+		'clientSecret': 'ba34d955d9f46d23bd3560c876adec73',
+		'callbackURL': 'http://www.laagendadelbebe.es/auth/facebook/callback'
+	},
+	'googleAuth' : {
+        'clientID'      : '472390506196-ed7d1ec9dtijbutaub04ae13bsdn2sv6.apps.googleusercontent.com',
+        'clientSecret'  : 'ny_1mC9wIXyyBOWfNwFSrJci',
+        'callbackURL'   : 'http://www.laagendadelbebe.es/auth/google/callback'
+    }
+}
